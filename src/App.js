e428a36df89b748179b3987e557b92ed9c34a432
Luis Ojeda <luisojeda27180@gmail.com>
2025-08-20 10:21:25 -0600

    Navigation component added.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="bg-blue-500 text-white p-8 text-center">
+        <h1 className="text-4xl font-bold">Luis Ojeda Portfolio</h1>
+        <p className="text-xl mt-4">React + Tailwind CSS!</p>
+      </div>
     </div>
   );
 }
